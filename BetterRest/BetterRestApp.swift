a790ae9f3660bbc5f68d015313605f7bc0b2d049
Weverton <wevertonmonteiroc@gmail.com>
2024-03-15 16:45:53 -0300

    First commit of project.

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by admin on 15/03/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterRest/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by admin on 15/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    var body: some View {
+        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+    }
+}
+
+#Preview {
+    ContentView()
+}
